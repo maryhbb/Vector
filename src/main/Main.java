ad6c27aa2afec57beddcc4d58655e6cc35182cce
Mehdi Salem <salem@axivion.com>
2021-03-15 23:45:31 +0100

    remove unncessary spacing from Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,17 +3,11 @@ package main;
 public class Main {
 
     public static void main(String[] args) {
-
         Vector2D a = new Vector2D(3, 7);
         Vector2D b = new Vector2D(5, 2);
 
         System.out.println(a.add(b));
         System.out.println(a.dotProduct(b));
         System.out.println(a.scale(5));
-
-
-
-
-
     }
 }
